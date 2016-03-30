2b09a73f265a63e7cba0794eaf617fa42cf89b05
Jim Lochhead <lochhead71@gmail.com>
2016-03-30 15:49:01 -0500

    Adding empty files for the Database folder within metalXchange.dev

---
+++ database/migration.php
@@ -0,0 +1,5 @@
+<?php 
+
+// this will be the base model for the migration files for all of our tables
+
+ ?>
---
+++ database/seeder.php
@@ -0,0 +1,5 @@
+<?php 
+
+// this is the base model for our seeder files for our various tables
+
+ ?>
