fd3673258627fbae116aa872ace4c9d4cfd989ee
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-31 11:09:24 -0500

    Basic HTML structure for user creation

--- public/users.create.php
+++ public/users.create.php
@@ -3,3 +3,30 @@
 // form for creating a new user
 
  ?>
+
+
+ <!doctype html>
+
+ <html>
+ <head>
+ 	<title>user.create.php</title>
+ 	<link rel="stylesheet" type="text/css" href="/css/bootstrap_css/bootstrap.css">
+ </head>
+ <body>
+ 	<div class="input-group">
+	 	<span class="input-group-addon"></span>
+	 	<input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+	</div>
+
+	<div class="input-group">
+	 	<span class="input-group-addon"></span>
+	 	<input type="text" class="form-control" placeholder="email" aria-describedby="basic-addon2">
+	</div>
+
+	<div class="input-group">
+	 	<span class="input-group-addon"></span>
+	 	<input type="text" class="form-control" placeholder="password" aria-describedby="basic-addon3">
+	</div>
+
+ </body>
+ </html>
