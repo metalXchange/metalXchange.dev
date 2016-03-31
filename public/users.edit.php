4ad8457c3b4ce006efb20f1f1ca1b1d7e42bf2a9
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-31 11:16:02 -0500

    Basic HTML structure for user profile edit

--- public/users.edit.php
+++ public/users.edit.php
@@ -3,3 +3,29 @@
 // form for editing a user profile
 
  ?>
+
+ <!doctype html>
+
+ <html>
+ <head>
+ 	<title>user.create.php</title>
+ 	<link rel="stylesheet" type="text/css" href="/css/bootstrap_css/bootstrap.css">
+ </head>
+ <body>
+ 	<div class="input-group">
+	 	<span class="input-group-addon"></span>
+	 	<input type="text" class="form-control" placeholder="(This will display the user's current username)" aria-describedby="basic-addon1">
+	</div>
+
+	<div class="input-group">
+	 	<span class="input-group-addon"></span>
+	 	<input type="text" class="form-control" placeholder="(And current email)" aria-describedby="basic-addon2">
+	</div>
+
+	<div class="input-group">
+	 	<span class="input-group-addon"></span>
+	 	<input type="text" class="form-control" placeholder="(And current password in password type)" aria-describedby="basic-addon3">
+	</div>
+
+ </body>
+ </html>
