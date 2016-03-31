a905842e3afd7547254bbd05636382d4e0397eb9
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-31 13:11:04 -0500

    Added "description" to user.edit

--- public/users.edit.php
+++ public/users.edit.php
@@ -27,5 +27,10 @@
 	 	<input type="text" class="form-control" placeholder="(And current password in password type)" aria-describedby="basic-addon3">
 	</div>
 
+	<div class="input-group">
+	 	<span class="input-group-addon"></span>
+	 	<input type="text" class="form-control" placeholder="(And current description/bio)" aria-describedby="basic-addon4">
+	</div>
+
  </body>
  </html>
