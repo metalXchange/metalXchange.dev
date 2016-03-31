c0211aa3110bfaeb207c6912e190fbc60fbf75ed
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-31 15:52:08 -0500

    Add basic structure to auth.logout.php

--- public/auth.logout.php
+++ public/auth.logout.php
@@ -3,3 +3,36 @@
 // log out method with redirect back to index
 
  ?>
+
+
+ <!doctype html>
+
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>metalXchange: acoutrements for the heavy metal lifestyle</title>
+ 	<link rel="stylesheet" href="/css/main.css">
+ 	<link rel="stylesheet" href="/css/bootstrap.css">
+ </head>
+ <body>
+	<?php include '../views/partials/header.php' ?>
+	<?php include '../views/partials/navbar.php' ?>
+
+
+	<!-- Since we haven't really figured out how our logout will work, I don't know what HTML structure to 
+	include here. -->
+
+
+	<?php include '../views/partials/footer.php' ?>
+
+	<script src="/js/jquery-1.12.0.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+	<script src="/js/main.js"></script>
+ 	<script>
+
+ 		// this will include any necessary embedded javascript
+
+ 	</script>
+
+ </body>
+ </html>
