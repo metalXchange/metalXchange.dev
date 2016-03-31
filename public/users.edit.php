ebae4751a46ed1e62217fc98d8d925081027a48a
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-31 14:33:03 -0500

    Finish adding head and script tags

--- public/users.edit.php
+++ public/users.edit.php
@@ -6,12 +6,17 @@
 
  <!doctype html>
 
- <html>
+ <html lang="en">
  <head>
- 	<title>user.create.php</title>
- 	<link rel="stylesheet" type="text/css" href="/css/bootstrap_css/bootstrap.css">
+ 	<meta charset="UTF-8">
+ 	<title>metalXchange: acoutrements for the heavy metal lifestyle</title>
+ 	<link rel="stylesheet" href="/css/main.css">
+ 	<link rel="stylesheet" href="/css/bootstrap.css">
  </head>
  <body>
+	<?php include '../views/partials/header.php' ?>
+	<?php include '../views/partials/navbar.php' ?>
+	
  	<div class="input-group">
 	 	<span class="input-group-addon"></span>
 	 	<input type="text" class="form-control" placeholder="(This will display the user's current username)" aria-describedby="basic-addon1">
@@ -32,5 +37,15 @@
 	 	<input type="text" class="form-control" placeholder="(And current description/bio)" aria-describedby="basic-addon4">
 	</div>
 
+	<?php include '../views/partials/footer.php' ?>
+
+	<script src="/js/jquery-1.12.0.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+	<script src="/js/main.js"></script>
+ 	<script>
+
+ 		// this will include any necessary embedded javascript
+
+ 	</script>
  </body>
  </html>
--- public/users.show.php
+++ public/users.show.php
@@ -7,12 +7,17 @@
 
  <!doctype html>
 
- <html>
+ <html lang="en">
  <head>
- 	<title>user.show.php</title>
- 	<link rel="stylesheet" type="text/css" href="/css/bootstrap_css/bootstrap.css">
+ 	<meta charset="UTF-8">
+ 	<title>metalXchange: acoutrements for the heavy metal lifestyle</title>
+ 	<link rel="stylesheet" href="/css/main.css">
+ 	<link rel="stylesheet" href="/css/bootstrap.css">
  </head>
  <body>
+	<?php include '../views/partials/header.php' ?>
+	<?php include '../views/partials/navbar.php' ?>
+	
  	<p>First name</p>
 
  	<p>Last name</p>
@@ -22,6 +27,17 @@
  	<p>Email</p>
 
  	<p>Bio/description</p>
+
+ 	<?php include '../views/partials/footer.php' ?>
+
+	<script src="/js/jquery-1.12.0.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+	<script src="/js/main.js"></script>
+ 	<script>
+
+ 		// this will include any necessary embedded javascript
+
+ 	</script>
  	
  </body>
  </html>
