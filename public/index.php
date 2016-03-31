c11aa504cd89d352ef398b4c1f13b39d628236f4
Jim Lochhead <lochhead71@gmail.com>
2016-03-31 11:15:33 -0500

    The index.php file with required component files for header, navbar and footer. I've also included a hidden resource folder with fonts and the Illustrator working file for the logo.

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php 
+
+// this is where any necessary embedded PHP will be included
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>metalXchange: acoutrements for the heavy metal lifestyle</title>
+ 	<link rel="stylesheet" href="/css/main.css">
+ 	<link rel="stylesheet" href="/css/bootstrap.css">
+ </head>
+ <body>
+	<?php include 'header.php' ?>
+	<?php include 'navbar.php' ?>
+
+	<main>
+		
+	</main>
+
+	<?php include 'footer.php' ?>
+
+	<script src="/js/jquery-2.2.0.min.js"></script>
+	<script src="/js/main.js"></script>
+ 	<script>
+
+ 		// this will include any necessary embedded javascript
+
+ 	</script>
+ </body>
+ </html>
