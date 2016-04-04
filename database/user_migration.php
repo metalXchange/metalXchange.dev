2cc6745fb83230c5d2aa2ebd1ea2bbd98f073661
PlatonicPoohBear <biologynut10@hotmail.com>
2016-04-04 13:37:19 -0500

    Implemented user_migration edits

--- database/user_migration.php
+++ database/user_migration.php
@@ -6,19 +6,19 @@
 
 $query = "CREATE TABLE users (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	is_admin BOOLEAN NOT NULL,
-	join_date DATE NOT NULL,
-	first_name VARCHAR(100) NOT NULL,
-	last_name VARCHAR(150) NOT NULL,
-	user_name VARCHAR(50) NOT NULL,
-	email VARCHAR(200) NOT NULL,
-	band_name VARCHAR(200) NOT NULL,
-	instrument VARCHAR(150) NOT NULL,
-	soul_possession BOOLEAN NOT NULL,
-	bio TEXT,
-	image_url VARCHAR(250),
-	PRIMARY KEY (id)
-	)";
+    join_date DATE NOT NULL,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    user_name VARCHAR(25) NOT NULL,
+    password VARCHAR(25) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    band_name VARCHAR(50) NOT NULL,
+    instrument VARCHAR(50) NOT NULL,
+    soul_possession TINYINT NOT NULL,
+    bio TEXT,
+    image_url VARCHAR(100),
+    PRIMARY KEY (id)
+    )";
 
 $dbc->exec($query);
 
