f3c245cf6044145b6349d37a6e0404dadef8fbf5
Jim Lochhead <lochhead71@gmail.com>
2016-04-05 15:27:28 -0500

    fixed some values in the sub-categories columns to match up with our $_GET requests.

--- database/pyrotechnics_seeder.php
+++ database/pyrotechnics_seeder.php
@@ -1,10 +1,10 @@
 <?php
 
 $pyrotechnics = array(
-	array ('user_id'=>4, 'implement_type'=>'Smoke Pot', 'brand'=>'Smoke Fountain', 'homemade'=>0, 'casualties'=>'none', 'item_description'=>'4 different colors; lasts up to 2 minutes', 'price'=>10, 'trade'=>1, 'image_url'=>'/img/uploads/pyr_SmokeFountain.jpg'),
-	array ('user_id'=>8, 'implement_type'=>'Concussion', 'brand'=>'El Especial', 'homemade'=>1, 'casualties'=>'I\'ll check when my ears stop ringing.', 'item_description'=>'I put 4 M80\'s in paint can... seemed liked a good item at the time.', 'price'=>100, 'trade'=>1, 'image_url'=>'/img/uploads/pyr_concussion.jpg'),
-	array ('user_id'=>2, 'implement_type'=>'Fireballs', 'brand'=>'CHAMELEON', 'homemade'=>0, 'casualties'=>'None', 'item_description'=>'Using a re-loadable canister system, the Chameleon performs just like a propane system but at a fraction of the cost and set-up time. Only used once.', 'price'=>2000, 'trade'=>0, 'image_url'=>'/img/uploads/pyr_SmokeFountain.jpg'),
-	array ('user_id'=>5, 'implement_type'=>'Fire Projector', 'brand'=>'Homemade', 'homemade'=>1, 'casualties'=>'A few... nothing to see here.', 'item_description'=>'My lawyer advised me it\'s best to plead the fifth on this...', 'price'=>500, 'trade'=>1, 'image_url'=>'/img/uploads/pyr_flameProjector.jpg')
+	array ('user_id'=>4, 'implement_type'=>'smoke_pot', 'brand'=>'Smoke Fountain', 'homemade'=>0, 'casualties'=>'none', 'item_description'=>'4 different colors; lasts up to 2 minutes', 'price'=>10, 'trade'=>1, 'image_url'=>'/img/uploads/pyr_SmokeFountain.jpg'),
+	array ('user_id'=>8, 'implement_type'=>'concussion', 'brand'=>'El Especial', 'homemade'=>1, 'casualties'=>'I\'ll check when my ears stop ringing.', 'item_description'=>'I put 4 M80\'s in paint can... seemed liked a good item at the time.', 'price'=>100, 'trade'=>1, 'image_url'=>'/img/uploads/pyr_concussion.jpg'),
+	array ('user_id'=>2, 'implement_type'=>'fireballs', 'brand'=>'CHAMELEON', 'homemade'=>0, 'casualties'=>'None', 'item_description'=>'Using a re-loadable canister system, the Chameleon performs just like a propane system but at a fraction of the cost and set-up time. Only used once.', 'price'=>2000, 'trade'=>0, 'image_url'=>'/img/uploads/pyr_SmokeFountain.jpg'),
+	array ('user_id'=>5, 'implement_type'=>'fire_projector', 'brand'=>'Homemade', 'homemade'=>1, 'casualties'=>'A few... nothing to see here.', 'item_description'=>'My lawyer advised me it\'s best to plead the fifth on this...', 'price'=>500, 'trade'=>1, 'image_url'=>'/img/uploads/pyr_flameProjector.jpg')
 );
 
 $stmt = $dbc->prepare('INSERT INTO pyrotechnics (user_id, implement_type, brand, homemade, casualties, item_description, price, trade, image_url) VALUES (:user_id, :implement_type, :brand, :homemade, :casualties, :item_description, :price, :trade, :image_url)');
