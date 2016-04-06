7c3d980bce6d4267ea6077f5e0627bb06dba4b3d
PlatonicPoohBear <biologynut10@hotmail.com>
2016-04-05 20:17:35 -0500

    Finished user.create. Changed User model and Auth class to allow table insertion.

--- models/User.php
+++ models/User.php
@@ -10,19 +10,24 @@ class User extends Model
     protected function insert()
     {
      
-        $stmt = $dbc->prepare('INSERT INTO users (join_date, first_name, last_name, user_name, password, email, band_name, instrument, soul_possession, bio, image_url) VALUES (:join_date, :first_name, :last_name, :user_name, :password, :email, :band_name, :instrument, :soul_possession, :bio, :image_url)');
+        $stmt = self::$dbc->prepare('INSERT INTO users (join_date, first_name, last_name, user_name, password, email, band_name, instrument, soul_possession, bio, image_url) VALUES (:join_date, :first_name, :last_name, :user_name, :password, :email, :band_name, :instrument, :soul_possession, :bio, :image_url)');
 
-        $stmt->bindValue(':join_date',  $item['join_date'],  PDO::PARAM_STR);
-        $stmt->bindValue(':first_name',  $item['first_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':last_name',  $item['last_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':user_name',  $item['user_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':password',  $item['password'],  PDO::PARAM_STR);
-        $stmt->bindValue(':email',  $item['email'],  PDO::PARAM_STR);
-        $stmt->bindValue(':band_name',  $item['band_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':instrument',  $item['instrument'],  PDO::PARAM_STR);
-        $stmt->bindValue(':soul_possession',  $item['soul_possession'],  PDO::PARAM_STR);
-        $stmt->bindValue(':bio',  $item['bio'],  PDO::PARAM_STR);
-        $stmt->bindValue(':image_url',  $item['image_url'],  PDO::PARAM_STR);
+            // TEMPORARY TEMPORARY TEMPORARY
+        date_default_timezone_set('America/Chicago');
+        $this->attributes['join_date'] = date('Y-m-d');
+
+
+        $stmt->bindValue(':join_date',  $this->attributes['join_date'],  PDO::PARAM_STR);
+        $stmt->bindValue(':first_name',  $this->attributes['first_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':last_name',  $this->attributes['last_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':user_name',  $this->attributes['user_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':password',  $this->attributes['password'],  PDO::PARAM_STR);
+        $stmt->bindValue(':email',  $this->attributes['email'],  PDO::PARAM_STR);
+        $stmt->bindValue(':band_name',  $this->attributes['band_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':instrument',  $this->attributes['instrument'],  PDO::PARAM_STR);
+        $stmt->bindValue(':soul_possession',  $this->attributes['soul_possession'],  PDO::PARAM_STR);
+        $stmt->bindValue(':bio',  $this->attributes['bio'],  PDO::PARAM_STR);
+        $stmt->bindValue(':image_url',  $this->attributes['image_url'],  PDO::PARAM_STR);
 
         $stmt->execute();
 
@@ -37,18 +42,18 @@ class User extends Model
     {
         $stmt = self::$dbc->prepare('UPDATE users (id, join_date, first_name, last_name, user_name, password, email, band_name, instrument, soul_possession, bio, image_url) VALUES (:id, :join_date, :first_name, :last_name, :user_name, :password, :email, :band_name, :instrument, :soul_possession, :bio, :image_url)');
 
-        $stmt->bindValue(':id',  $item['id'],  PDO::PARAM_STR);
-        $stmt->bindValue(':join_date',  $item['join_date'],  PDO::PARAM_STR);
-        $stmt->bindValue(':first_name',  $item['first_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':last_name',  $item['last_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':user_name',  $item['user_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':password',  $item['password'],  PDO::PARAM_STR);
-        $stmt->bindValue(':email',  $item['email'],  PDO::PARAM_STR);
-        $stmt->bindValue(':band_name',  $item['band_name'],  PDO::PARAM_STR);
-        $stmt->bindValue(':instrument',  $item['instrument'],  PDO::PARAM_STR);
-        $stmt->bindValue(':soul_possession',  $item['soul_possession'],  PDO::PARAM_STR);
-        $stmt->bindValue(':bio',  $item['bio'],  PDO::PARAM_STR);
-        $stmt->bindValue(':image_url',  $item['image_url'],  PDO::PARAM_STR);
+        $stmt->bindValue(':id',  $this->attributes['id'],  PDO::PARAM_STR);
+        $stmt->bindValue(':join_date',  $this->attributes['join_date'],  PDO::PARAM_STR);
+        $stmt->bindValue(':first_name',  $this->attributes['first_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':last_name',  $this->attributes['last_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':user_name',  $this->attributes['user_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':password',  $this->attributes['password'],  PDO::PARAM_STR);
+        $stmt->bindValue(':email',  $this->attributes['email'],  PDO::PARAM_STR);
+        $stmt->bindValue(':band_name',  $this->attributes['band_name'],  PDO::PARAM_STR);
+        $stmt->bindValue(':instrument',  $this->attributes['instrument'],  PDO::PARAM_STR);
+        $stmt->bindValue(':soul_possession',  $this->attributes['soul_possession'],  PDO::PARAM_STR);
+        $stmt->bindValue(':bio',  $this->attributes['bio'],  PDO::PARAM_STR);
+        $stmt->bindValue(':image_url',  $this->attributes['image_url'],  PDO::PARAM_STR);
 
         $stmt->execute();
 
--- utils/Auth.php
+++ utils/Auth.php
@@ -87,8 +87,6 @@ class Auth
         }
         // Finally, destroy the session.
         session_destroy();
-
-        header('Location: /index.php');
     }
    
 
